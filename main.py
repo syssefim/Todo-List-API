b03483df7b59886760bd6a02ad648a15a134030e
syssefim <serafim.sharkov@gmail.com>
2026-03-02 20:08:37 -0800

    started learning FastAPI

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+items = []
+
+
+@app.get("/")
+def root():
+    return {"Hello": "World"}
+
+
+@app.post("/items")
+def create_items(item: str):
+    items.append(item)
+    return items
+
+
+@app.get("/items/{item_id}")
+def get_item(item_id: int) -> str:
+    item = items[item_id]
+    return item
